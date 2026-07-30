f64f2c919993974cb4ca602f8ed150dd8f0aa5c3
VanLH23 <vanlh23@fpt.com>
2026-07-30 11:13:03 +0700

    Init apple app

---
+++ Table/TableApp.swift
@@ -0,0 +1,17 @@
+//
+//  TableApp.swift
+//  Table
+//
+//  Created by Handsome Mac on 30/7/26.
+//
+
+import SwiftUI
+
+@main
+struct TableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
